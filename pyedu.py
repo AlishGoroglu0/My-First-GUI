6cc4c8001dc0bd2ca983c71cc42704e478d210e2
Aliş Göroğlu <Alishgoroglu@hotmail.com>
2025-07-26 06:43:09 +0300

    Add files via upload

--- pyedu.py
+++ pyedu.py
@@ -1,50 +1,38 @@
 from tkinter import *
 
-Count = 0
 
-def label_appear():
-    global Count
-    if Count >= 10:
-        lab2.pack()
-    elif Count <= -10:
-        lab2.pack()
-    else: 
-        lab2.pack_forget()
+expression = ""
 
-def count_up():
-    global Count
-    Count += 1
-    lab.config(text=f"現在の数は{Count}です")
-    label_appear()
-def count_down():
-    global Count
-    Count -= 1
-    lab.config(text=f"現在の数は{Count}です")
-    label_appear()
-
+def expression_adder(expr):
+    global expression
+    if expr == "=":
+        expression = eval(expression)
+        Lab.config(text=(f"Hello this is a basic Calculator\nYOUR EXPRESSION\n{expression}"))
+    else:   
+        expression += expr
+        Lab.config(text=(f"Hello this is a basic Calculator\nYOUR EXPRESSION\n{expression}"))
+    
 
 window = Tk()
 window.geometry("500x500")
 window.config(background="gray")
 
+Lab = Label(text=f"Hello this is a basic Calculator\nYOUR EXPRESSION\n{expression}",
+            font=("helvetica", 20 , "bold") , bg="white" , fg="Orange",
+            relief="raised", bd=10 , pady=10 , anchor="n"
+            
+            )
 
+b_1 = Button(text="1", command=lambda: expression_adder("1"))
+b_2 = Button(text="2", command=lambda: expression_adder("2"))
+b_p = Button(text="+", command=lambda: expression_adder("+"))
+b_e = Button(text="=", command=lambda: expression_adder("="))
 
+Lab.pack()
+b_1.pack()
+b_2.pack()
+b_p.pack()
+b_e.pack()
 
-but = Button(text="1つ増える" , font=("Arial" , 30 , "bold"), cursor="dot",
-            activebackground="green", activeforeground="white" , command=count_up)
-but2 = Button(text="1つ減る" , font=("Arial" , 30 , "bold"), cursor="dot",
-            activebackground="red", activeforeground="white" , command=count_down)
-lab = Label(text=f"現在の数は{Count}です" ,
-            font=("Arial" , 40 , "bold"), padx=5 , pady=5,
-            relief="solid", bd=6,
-            cursor="arrow",)
-lab2 = Label(text=f"あなたの数字は10で割り切れます{Count}です" ,
-            font=("Arial" , 40 , "bold"), padx=5 , pady=5,
-            relief="solid", bd=6,
-            cursor="arrow",)
-
-lab.pack()
-but.pack()
-but2.pack()
 
 window.mainloop()
